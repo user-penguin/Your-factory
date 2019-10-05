dfe5f5080ef7f923ca39f7a7dd8fd19f0e175055
user-penguin <kobzev96@gmail.com>
2019-10-05 18:42:46 +0700

    add person entity and doctrine-orm bundle

--- back_side/src/Controller/BaseController.php
+++ back_side/src/Controller/BaseController.php
@@ -3,11 +3,20 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 
 class BaseController extends AbstractController
 {
+    /**
+     * @return Response
+     */
     public function index()
     {
         return $this->render('InformationLayer/index.html.twig');
+    }
+
+    public function addPerson()
+    {
+
     }
 }
