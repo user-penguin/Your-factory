05a54511cc378707b11aedc1e064326a2ae1cc89
user-penguin <kobzev96@gmail.com>
2019-10-05 18:27:14 +0700

    Add bootstrap, Twig bundle, base page

---
+++ back_side/src/Controller/BaseController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class BaseController extends AbstractController
+{
+    public function index()
+    {
+        return $this->render('InformationLayer/index.html.twig');
+    }
+}
