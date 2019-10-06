3c365d1e715f284127512363f8a4ff6fe33e5d42
user-penguin <kobzev96@gmail.com>
2019-10-06 20:49:10 +0700

    change fields in Person

--- back_side/src/Entity/Person.php
+++ back_side/src/Entity/Person.php
@@ -6,6 +6,7 @@ use Doctrine\ORM\Mapping as ORM;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\PersonRepository")
+ * @ORM\Table(name="persons")
  */
 class Person
 {
@@ -15,6 +16,100 @@ class Person
      * @ORM\Column(type="integer")
      */
     private $id;
+
+    /**
+     * @var string $firstName
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $firstName;
+
+    /**
+     * @var string $lastName
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $lastName;
+
+    /**
+     * @var string $patronymic
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $patronymic;
+
+    /**
+     * @ORM\Column(type="string", columnDefinition="ENUM('student', 'parent', 'employee')")
+     */
+    private $role;
+
+    /**
+     * @var string $phone
+     * @ORM\Column(type="string", length=20, nullable=false)
+     */
+    private $phone;
+
+    /**
+     * @return string
+     */
+    public function getFirstName(): string
+    {
+        return $this->firstName;
+    }
+
+    /**
+     * @param string $firstName
+     */
+    public function setFirstName(string $firstName): void
+    {
+        $this->firstName = $firstName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLastName(): string
+    {
+        return $this->lastName;
+    }
+
+    /**
+     * @param string $lastName
+     */
+    public function setLastName(string $lastName): void
+    {
+        $this->lastName = $lastName;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPatronymic(): string
+    {
+        return $this->patronymic;
+    }
+
+    /**
+     * @param string $patronymic
+     */
+    public function setPatronymic(string $patronymic): void
+    {
+        $this->patronymic = $patronymic;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPhone(): string
+    {
+        return $this->phone;
+    }
+
+    /**
+     * @param string $phone
+     */
+    public function setPhone(string $phone): void
+    {
+        $this->phone = $phone;
+    }
+
 
     public function getId(): ?int
     {
